1513db9f0f210070438fa44a5e339c61279d258d
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2022-05-31 17:08:45 -0300

    Terminando tela *Novo Contato*

--- 1 - AdegaI/src/adegai/ContatosADM.java
+++ 1 - AdegaI/src/adegai/ContatosADM.java
@@ -6,7 +6,10 @@ public class ContatosADM extends javax.swing.JFrame {
     
     public ContatosADM() {
         initComponents();
-
+        
+        showOffCliente(); 
+        showOffFuncionario();
+        
     }
 
     public ContatosADM(String funcionario, String funcao){
@@ -19,6 +22,46 @@ public class ContatosADM extends javax.swing.JFrame {
         botaoVendas.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         botaoHome.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         botaoRelatorios.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    }
+    
+    //SHOW NO CLIENTE
+    public void showCliente() {
+        novoClienteCaixa.show(true);
+        novoClienteCaixa.enable();
+        nomeCliente.show(true);
+        nomeCliente.enable();
+        nomeCliente.setText("Nome...");
+    }
+    
+    //SHOW OFF NO CLIENTE
+    public void showOffCliente() {
+        novoClienteCaixa.show(false);
+        novoClienteCaixa.disable();
+        nomeCliente.show(false);
+        nomeCliente.disable();
+        nomeCliente.setText(null);
+    }
+    
+    //SHOW OFF NO FUNCIONARIO
+    public void showOffFuncionario(){
+        nomeFuncionario.show(false);
+        nomeFuncionario.setText(null);
+        loginFuncionario.show(false);
+        loginFuncionario.setText(null);
+        senhaFuncionario.show(false);
+        senhaFuncionario.setText(null);
+        novoFuncionarioCaixa.show(false);
+    }
+    
+    //SHOW NO FUNCIONARIO
+    public void showFuncionario() {
+        nomeFuncionario.show(true);
+        nomeFuncionario.setText("Nome...");
+        loginFuncionario.show(true);
+        loginFuncionario.setText("Login...");
+        senhaFuncionario.show(true);
+        senhaFuncionario.setText("Senha...");
+        novoFuncionarioCaixa.show(true);
     }
     
     @SuppressWarnings("unchecked")
@@ -37,6 +80,18 @@ public class ContatosADM extends javax.swing.JFrame {
         botaoRelatorios = new javax.swing.JButton();
         menuLateral = new javax.swing.JLabel();
         menuCima = new javax.swing.JLabel();
+        nomeFuncionario = new javax.swing.JTextField();
+        loginFuncionario = new javax.swing.JTextField();
+        senhaFuncionario = new javax.swing.JTextField();
+        novoFuncionarioCaixa = new javax.swing.JLabel();
+        nomeCliente = new javax.swing.JTextField();
+        novoClienteCaixa = new javax.swing.JLabel();
+        selecionarBotao = new javax.swing.JButton();
+        comboContato = new javax.swing.JComboBox<>();
+        cadastrarBotao = new javax.swing.JButton();
+        limparBotao = new javax.swing.JButton();
+        novoContatoCaixa = new javax.swing.JLabel();
+        todosContatosCaixa = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -125,6 +180,94 @@ public class ContatosADM extends javax.swing.JFrame {
         menuCima.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/HomeADM/menuCima.png"))); // NOI18N
         jPanel1.add(menuCima, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
+        nomeFuncionario.setBackground(new java.awt.Color(255, 255, 255));
+        nomeFuncionario.setFont(new java.awt.Font("Jost", 1, 20)); // NOI18N
+        nomeFuncionario.setForeground(new java.awt.Color(32, 32, 32));
+        nomeFuncionario.setText("Nome");
+        nomeFuncionario.setBorder(null);
+        jPanel1.add(nomeFuncionario, new org.netbeans.lib.awtextra.AbsoluteConstraints(271, 365, 320, 40));
+
+        loginFuncionario.setBackground(new java.awt.Color(255, 255, 255));
+        loginFuncionario.setFont(new java.awt.Font("Jost", 1, 20)); // NOI18N
+        loginFuncionario.setForeground(new java.awt.Color(32, 32, 32));
+        loginFuncionario.setText("Login");
+        loginFuncionario.setBorder(null);
+        jPanel1.add(loginFuncionario, new org.netbeans.lib.awtextra.AbsoluteConstraints(271, 453, 320, 40));
+
+        senhaFuncionario.setBackground(new java.awt.Color(255, 255, 255));
+        senhaFuncionario.setFont(new java.awt.Font("Jost", 1, 20)); // NOI18N
+        senhaFuncionario.setForeground(new java.awt.Color(32, 32, 32));
+        senhaFuncionario.setText("Senha");
+        senhaFuncionario.setBorder(null);
+        jPanel1.add(senhaFuncionario, new org.netbeans.lib.awtextra.AbsoluteConstraints(271, 550, 320, 40));
+
+        novoFuncionarioCaixa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/caixaFuncionario.png"))); // NOI18N
+        jPanel1.add(novoFuncionarioCaixa, new org.netbeans.lib.awtextra.AbsoluteConstraints(224, 328, -1, -1));
+
+        nomeCliente.setBackground(new java.awt.Color(255, 255, 255));
+        nomeCliente.setFont(new java.awt.Font("Jost", 1, 24)); // NOI18N
+        nomeCliente.setForeground(new java.awt.Color(32, 32, 32));
+        nomeCliente.setText("Nome...");
+        nomeCliente.setBorder(null);
+        jPanel1.add(nomeCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 405, 410, 50));
+
+        novoClienteCaixa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/nomeCliente.png"))); // NOI18N
+        jPanel1.add(novoClienteCaixa, new org.netbeans.lib.awtextra.AbsoluteConstraints(175, 360, -1, -1));
+
+        selecionarBotao.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoSelecionar.png"))); // NOI18N
+        selecionarBotao.setBorder(null);
+        selecionarBotao.setContentAreaFilled(false);
+        selecionarBotao.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        selecionarBotao.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoSelecionar.png"))); // NOI18N
+        selecionarBotao.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoSelecionarPressed.png"))); // NOI18N
+        selecionarBotao.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                selecionarBotaoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(selecionarBotao, new org.netbeans.lib.awtextra.AbsoluteConstraints(544, 271, -1, -1));
+
+        comboContato.setBackground(new java.awt.Color(255, 255, 255));
+        comboContato.setFont(new java.awt.Font("Jost", 0, 12)); // NOI18N
+        comboContato.setForeground(new java.awt.Color(0, 0, 0));
+        comboContato.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Cliente", "Funcionário", "Administrador" }));
+        comboContato.setBorder(null);
+        comboContato.setMinimumSize(new java.awt.Dimension(106, 40));
+        comboContato.setPreferredSize(new java.awt.Dimension(106, 40));
+        jPanel1.add(comboContato, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 271, 180, 17));
+
+        cadastrarBotao.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoCadastrar.png"))); // NOI18N
+        cadastrarBotao.setBorder(null);
+        cadastrarBotao.setContentAreaFilled(false);
+        cadastrarBotao.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        cadastrarBotao.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoCadastrar.png"))); // NOI18N
+        cadastrarBotao.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoCadastrarPressed.png"))); // NOI18N
+        cadastrarBotao.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cadastrarBotaoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(cadastrarBotao, new org.netbeans.lib.awtextra.AbsoluteConstraints(471, 636, -1, -1));
+
+        limparBotao.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoLimpar.png"))); // NOI18N
+        limparBotao.setBorder(null);
+        limparBotao.setContentAreaFilled(false);
+        limparBotao.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        limparBotao.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoLimpar.png"))); // NOI18N
+        limparBotao.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/botaoLimparPressed.png"))); // NOI18N
+        limparBotao.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                limparBotaoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(limparBotao, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 636, -1, -1));
+
+        novoContatoCaixa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/novoContato.png"))); // NOI18N
+        jPanel1.add(novoContatoCaixa, new org.netbeans.lib.awtextra.AbsoluteConstraints(143, 138, -1, -1));
+
+        todosContatosCaixa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/adegai/ContatosADM/caixaContatos.png"))); // NOI18N
+        jPanel1.add(todosContatosCaixa, new org.netbeans.lib.awtextra.AbsoluteConstraints(715, 138, -1, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -164,6 +307,39 @@ public class ContatosADM extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_botaoProdutosActionPerformed
 
+    private void cadastrarBotaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastrarBotaoActionPerformed
+
+        
+    }//GEN-LAST:event_cadastrarBotaoActionPerformed
+
+    private void selecionarBotaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_selecionarBotaoActionPerformed
+        
+        if(comboContato.getSelectedItem().toString().equals("Administrador")){
+            
+            showOffFuncionario();
+            showOffCliente();
+            
+        } else if (comboContato.getSelectedItem().toString().equals("Cliente")) { 
+            
+            showCliente();
+            
+            showOffFuncionario();
+                        
+        } else {
+            
+            showFuncionario();
+            
+            showOffCliente();
+        }
+    }//GEN-LAST:event_selecionarBotaoActionPerformed
+
+    private void limparBotaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limparBotaoActionPerformed
+        
+        showOffCliente();
+        showOffFuncionario();
+        
+    }//GEN-LAST:event_limparBotaoActionPerformed
+
     public static void main(String args[]) {
         
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -179,12 +355,24 @@ public class ContatosADM extends javax.swing.JFrame {
     private javax.swing.JButton botaoProdutos;
     private javax.swing.JButton botaoRelatorios;
     private javax.swing.JButton botaoVendas;
+    private javax.swing.JButton cadastrarBotao;
+    private javax.swing.JComboBox<String> comboContato;
     private javax.swing.JLabel funcionarioFunction;
     private javax.swing.JLabel funcionarioNome;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton limparBotao;
+    private javax.swing.JTextField loginFuncionario;
     private javax.swing.JLabel logoIcon;
     private javax.swing.JLabel menuCima;
     private javax.swing.JLabel menuLateral;
+    private javax.swing.JTextField nomeCliente;
+    private javax.swing.JTextField nomeFuncionario;
+    private javax.swing.JLabel novoClienteCaixa;
+    private javax.swing.JLabel novoContatoCaixa;
+    private javax.swing.JLabel novoFuncionarioCaixa;
+    private javax.swing.JButton selecionarBotao;
+    private javax.swing.JTextField senhaFuncionario;
+    private javax.swing.JLabel todosContatosCaixa;
     private javax.swing.JLabel userIcon;
     // End of variables declaration//GEN-END:variables
 }
