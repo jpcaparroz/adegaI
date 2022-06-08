166c13a82f313bb7769565f1acd90d977e74653a
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2022-06-08 19:33:20 -0300

    script do banco de dados

--- 1 - AdegaI/src/adegai/RelatoriosADM.java
+++ 1 - AdegaI/src/adegai/RelatoriosADM.java
@@ -90,6 +90,7 @@ public class RelatoriosADM extends javax.swing.JFrame {
         bg = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
